4ad5ed0da43574d8f6d7c62760bc36af7045185b
GarrySh <sooo.lame@gmail.com>
2019-05-24 15:47:22 +1000

    add devtool

---
+++ Makefile
@@ -0,0 +1,11 @@
+start:
+	yarn run start
+
+test:
+	yarn run test
+
+build:
+	yarn run build
+
+install:
+	yarn install
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import DevTools from 'mobx-react-devtools';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+      <DevTools />
       <h1>Hello world!</h1>
     </div>
   );
-}
+};
 
 export default App;
